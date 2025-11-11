81d20e271a84f74d1bc7208d0b55ed20469298ed
duetocodes <duetocodes@outlook.com>
2025-11-11 21:35:13 +0800

    wip: use padding instead

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="relative
-      mx-auto  px-4 sm:px-6 mb-96 sm:mb-48 md:mb-16
+      mx-auto px-4 sm:px-6 pb-96 sm:pb-48 md:pb-16
       max-w-5xl w-full min-w-0 min-h-[100svh]
       overflow-auto">
     <slot />
