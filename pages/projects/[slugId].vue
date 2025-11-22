01593d9658eb7eee027cbcb7ff92f0ff298d40f9
duetocodes <duetocodes@outlook.com>
2025-11-22 23:11:10 +0800

    refactor(ssr): catch unpublished/unknown slug before fetching

--- pages/projects/[slugId].vue
+++ pages/projects/[slugId].vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <component
-      :is="activeSlug"
-      v-if="activeSlug" />
+      :is="assignedComponent"
+      v-if="assignedComponent" />
 
     <div
-      v-else
+      v-else-if="isPublishedInAtLeastOneLocale"
       class="mt-44 w-full flex justify-center items-center">
       <UAlert
-        :title="$t('ItemCurrentlyNotAvailable', { item: `${route.params.slugId}` })"
-        icon="material-symbols:settings-alert-outline"
+        :title="$t('ItemCurrentlyNotAvailable', { item: slugLabel })"
+        icon="material-symbols:construction"
         class="max-w-sm md:max-w-md"
         color="warning"
         variant="subtle">
@@ -37,13 +37,51 @@ import type { ProjectSchema } from '~/schemas';
 type Project = z.infer<typeof ProjectSchema>;
 type ProjectSlug = Pick<Project, 'id' | 'documentId' | 'slugId' | 'locale'>;
 
-const activeSlug = ref(null);
+const assignedComponent = ref(null);
 const nuxtApp = useNuxtApp();
 const localePath = useLocalePath();
 const route = useRoute();
 const { t: $t, locale } = useI18n();
 
-const { data: slugs } = await useFetch<{ data: ProjectSlug[] }>(
+const { data: cached } = useNuxtData<{ data: ProjectSlug[] }>('projects-published-slugs-all-locales');
+
+const isPublishedInAtLeastOneLocale = ref<ProjectSlug>();
+
+onBeforeMount(() => {
+  isPublishedInAtLeastOneLocale.value = cached.value?.data.find((item) => {
+    return item.slugId === route.params.slugId;
+  });
+
+  if (!cached.value) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: $t('UnexpectedErrorOccurred'),
+    });
+  }
+
+  if (!isPublishedInAtLeastOneLocale.value) {
+    // unknown slug
+    throw createError({
+      statusCode: 404,
+      statusMessage: $t('error.404') + `: /${route.params.slugId}`,
+    });
+  }
+});
+
+const slugLabel = computed(() => {
+  switch (route.params.slugId) {
+    case 'treasury-yield-visualiser':
+      return $t('TreasuryYieldVisualiser');
+    case 'currency-converter':
+      return $t('CurrencyConverter');
+    case 'typing-game':
+      return $t('TypingGame');
+    default:
+      return route.params.slugId;
+  }
+});
+
+const { data: renderedSlugs } = await useFetch<{ data: ProjectSlug[] }>(
   `/api/projects`,
   {
     method: 'GET',
@@ -59,24 +97,11 @@ const { data: slugs } = await useFetch<{ data: ProjectSlug[] }>(
   },
 );
 
-const isPublishedInCurrentLocale = slugs.value?.data.find((item) => {
+const isPublishedInCurrentLocale = renderedSlugs.value?.data.find((item) => {
   return item.slugId === route.params.slugId && item.locale === locale.value;
 });
 
 if (isPublishedInCurrentLocale) {
-  activeSlug.value = defineAsyncComponent(() => import(`~/components/Projects/${isPublishedInCurrentLocale.slugId}.vue`));
-}
-else {
-  const isPublishedInAtLeastOneLocale = slugs.value?.data.find((item) => {
-    return item.slugId === route.params.slugId;
-  });
-
-  if (!isPublishedInAtLeastOneLocale) {
-    // unknown slug
-    throw createError({
-      statusCode: 404,
-      statusMessage: $t('error.404'),
-    });
-  }
+  assignedComponent.value = defineAsyncComponent(() => import(`~/components/Projects/${isPublishedInCurrentLocale.slugId}.vue`));
 }
 </script>
