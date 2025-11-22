c700b827caa92bcdf7e0ff8eeb335e20ec566a1b
duetocodes <duetocodes@outlook.com>
2025-11-22 22:57:45 +0800

    refactor(config): change strapi `query` its consuming logic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,21 +2,13 @@
 
 import { $fetch } from 'ofetch';
 import { STRAPI_ENDPOINTS } from './server/utils/api';
-import type { LocaleObject } from '@nuxtjs/i18n';
 import type { ProjectSchema } from './schemas';
 import type { z } from 'zod';
 
 type Project = z.infer<typeof ProjectSchema>;
 type ProjectSlug = Pick<Project, 'id' | 'documentId' | 'slugId' | 'locale'>;
 
-const localeList: LocaleObject[] = [
-  { code: 'en', name: 'EN', dir: 'ltr', file: 'en.json', dateLocale: 'en-GB' },
-  { code: 'th', name: 'ไทย', dir: 'ltr', file: 'th.json', dateLocale: 'th-TH-u-ca-gregory' }, // for gregory calendar in Thai locale (its Buddhist calendar by default)
-  { code: 'tr', name: 'Türkçe', dir: 'ltr', file: 'tr.json', dateLocale: 'tr' },
-  { code: 'zh', name: '简体中文', dir: 'ltr', file: 'zh.json', dateLocale: 'zh' },
-];
-
-const fetchPublishedProjectSlugs = async (locale: string) => {
+const fetchPublishedProjectSlugs = async () => {
   return $fetch(
     process.env.NUXT_STRAPI_API_BASE + STRAPI_ENDPOINTS.Projects,
     {
@@ -24,11 +16,12 @@ const fetchPublishedProjectSlugs = async (locale: string) => {
         Authorization: `Bearer ${process.env.NUXT_STRAPI_READ_ONLY_TOKEN}`,
       },
       query: {
-        locale: locale,
+        locale: '*',
         fields: ['slugId', 'locale'],
       },
     },
-  ).then((res: { data: ProjectSlug[] }) => res.data);
+  )
+    .then((res: { data: ProjectSlug[] }) => res.data);
 };
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
@@ -88,23 +81,23 @@ export default defineNuxtConfig({
   },
   i18n: {
     defaultLocale: 'en',
-    locales: localeList,
+    locales: [
+      { code: 'en', name: 'EN', dir: 'ltr', file: 'en.json', dateLocale: 'en-GB' },
+      { code: 'th', name: 'ไทย', dir: 'ltr', file: 'th.json', dateLocale: 'th-TH-u-ca-gregory' }, // for gregory calendar in Thai locale (its Buddhist calendar by default)
+      { code: 'tr', name: 'Türkçe', dir: 'ltr', file: 'tr.json', dateLocale: 'tr' },
+      { code: 'zh', name: '简体中文', dir: 'ltr', file: 'zh.json', dateLocale: 'zh' },
+    ],
     lazy: true,
     strategy: 'prefix_except_default',
   },
   hooks: {
     // https://nuxt.com/docs/3.x/getting-started/prerendering#prerenderroutes-nuxt-hook
     async 'prerender:routes'(ctx) {
-      const locales = localeList.map((loc: LocaleObject) => loc.code);
+      const published = await fetchPublishedProjectSlugs();
 
-      locales.forEach(async (loc) => {
-        // to accomodate [slugId].vue, published/draft status is set for each locale individually
-        const response = await fetchPublishedProjectSlugs(loc);
-
-        response.forEach((slug: ProjectSlug) => {
-          const prefix = loc === 'en' ? '' : `/${loc}`;
-          ctx.routes.add(`${prefix}/projects/${slug.slugId}`);
-        });
+      published.forEach((slug: ProjectSlug) => {
+        const prefix = slug.locale === 'en' ? '' : `/${slug.locale}`;
+        ctx.routes.add(`${prefix}/projects/${slug.slugId}`);
       });
     },
   },
