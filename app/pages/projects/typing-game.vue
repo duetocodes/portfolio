52a042d2c2f3d27f0cb8e0c9b84191fccdc70f15
duetocodes <duetocodes@outlook.com>
2026-06-04 21:03:33 +0700

    fix(i18n): clear intlify warning

--- app/pages/projects/typing-game.vue
+++ app/pages/projects/typing-game.vue
@@ -164,7 +164,7 @@
             </span>
             <UTooltip
               v-if="feedbackData?.feedback"
-              :text="$t(TEXTS.AIGeneratedContent)"
+              :text="TEXTS.AIGeneratedContent"
               :delay-duration="200">
               <UBadge
                 class="select-none"
@@ -226,7 +226,7 @@
           {{ data.topic }}
         </span>
         <UTooltip
-          :text="$t(TEXTS.AIGeneratedContent)"
+          :text="TEXTS.AIGeneratedContent"
           :delay-duration="200">
           <UBadge
             class="select-none"
